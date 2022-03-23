b18e694ad2afce16a67830f58c331934897cc016
Dolapo <dxa6589@rit.edu>
2022-03-23 16:43:56 -0400

    Quite a bit. Read description
    Added more object glitches
    Added glitch material.
    Added mocap test animator
    Added bar props and animated dance asset packs

---
+++ Assets/Scripts/TestMotion.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMotion : MonoBehaviour
+{
+    public float accelerationRate, deccelerationRate, maxSpeed;
+    public bool kbMotion, msMotion;
+    public Vector3 acceleration, velocity, position, mousePos;
+    public Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        accelerationRate = 0.1f;
+        deccelerationRate = 0.1f;
+        maxSpeed = 1f;
+
+        acceleration = Vector3.zero;
+        velocity = Vector3.zero;
+        position = transform.position;
+
+        //RB = this.GetComponent<Rigidbody>();
+        mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (kbMotion)
+        {
+            CalculatePosition();
+            // move the vehicle to it's new position
+            transform.position = position;
+        }
+        if (msMotion)
+        {
+            FollowMouse();
+        }
+    }
+    void FixedUpdate()
+    {
+        if (msMotion) RB.MovePosition(position);
+    }
+
+    void CalculatePosition()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            acceleration = transform.forward * accelerationRate;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            velocity += acceleration;
+
+            Debug.Log("up arrow is down");
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            acceleration = -transform.forward * accelerationRate;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            velocity += acceleration;
+
+            Debug.Log("down arrow is down");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            acceleration = -transform.right * accelerationRate;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            velocity += acceleration;
+
+            Debug.Log("left arrow is down");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            acceleration = transform.right * accelerationRate;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            velocity += acceleration;
+
+            Debug.Log("right arrow is down");
+        }
+
+        if (!Input.GetKey(KeyCode.LeftArrow)&&!Input.GetKey(KeyCode.RightArrow)&&!Input.GetKey(KeyCode.UpArrow)&&!Input.GetKey(KeyCode.DownArrow))
+        {
+            // exponentially reduce the velocity 
+            velocity *= deccelerationRate;
+        }
+
+        // add velocity to the vehicle
+        position += velocity;
+    }
+
+    void FollowMouse()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position = Vector2.Lerp(transform.position, mousePos, maxSpeed);
+        position.z = transform.position.z;
+    }
+}
